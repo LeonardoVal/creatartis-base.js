b6d327a8cee794f3b89392e433908f0961946ee6
Leonardo Val <leonardo.val@creatartis.com>
2014-12-26 11:27:11 -0200

    math.js
    + Added factorial() function, with test cases.

--- src/math.js
+++ src/math.js
@@ -4,11 +4,24 @@ Mathematical and numerical functions and utilities.
 */
 var math = exports.math = {};
 
-// ## Probability. #############################################################
+// ## Combinatorics ################################################################################
 
-/** The probability density function (or PDF) of the normal (or gaussian)
-distribution. The parameters `mean` and `variance` default to the standard
-normal (i.e `mean=0` and `variance=1`).
+math.factorial = function factorial(n) {
+	n = n|0;
+	if (n < 0) {
+		return NaN;
+	} else {
+		for (var f = 1; n > 0; --n) {
+			f *= n;
+		}
+		return f;
+	}
+};
+
+// ## Probability ##################################################################################
+
+/** The probability density function (or PDF) of the normal (or gaussian) distribution. The 
+parameters `mean` and `variance` default to the standard normal (i.e `mean=0` and `variance=1`).
 */
 math.gauss_pdf = function gauss_pdf(value, mean, variance) {
 	mean = isNaN(mean) ? 0 : +mean;
@@ -19,9 +32,9 @@ math.gauss_pdf = function gauss_pdf(value, mean, variance) {
 		/ standardDeviation * Math.sqrt(2 * Math.PI);
 };
 
-/** Complementary error function routine based on Chebyshev fitting as explained
-in [Numerical Recipes in C (2nd edition)](http://www.nr.com/), with fractional 
-error everywhere less than 1.2e-7.
+/** Complementary error function routine based on Chebyshev fitting as explained in 
+[Numerical Recipes in C (2nd edition)](http://www.nr.com/), with fractional error everywhere less 
+than 1.2e-7.
 */
 math.gauss_erfc = function gauss_erfc(value) {
 	var z = Math.abs(value),
@@ -32,9 +45,8 @@ math.gauss_erfc = function gauss_erfc(value) {
     return value >= 0.0 ? ans : 2.0 - ans;
 };
 
-/** The cumulative density function (or CDF) of the normal (or gaussian)
-distribution. The parameters `mean` and `variance` default to the standard
-normal (i.e `mean=0` and `variance=1`).
+/** The cumulative density function (or CDF) of the normal (or gaussian) distribution. The 
+parameters `mean` and `variance` default to the standard normal (i.e `mean=0` and `variance=1`).
 */
 math.gauss_cdf = function gauss_cdf(value, mean, variance) {
 	mean = isNaN(mean) ? 0 : +mean;
---
+++ tests/specs/math.test.js
@@ -0,0 +1,14 @@
+﻿define(['base'], function (base) { "use strict";
+	var math = base.math;
+	
+	describe("math", function () {
+		it(".factorial()", function () {
+			expect(typeof math.factorial).toBe('function');
+			[[0,1], [1,1], [2,2], [3,6], [4,24], [5,120], [6,720]].forEach(function (test) {
+				expect(math.factorial(test[0])).toBe(test[1]);
+			});
+			expect(math.factorial(-1)).toBeNaN();
+		}); // factorial().
+		
+	}); //// describe math.
+});
