edb127f1c41dfb84bcb367cf4076cb25bda52727
Leonardo Val <leonardo.val@creatartis.com>
2015-01-24 21:57:19 -0200

    Test cases for math.sign() and math.clamp()

--- tests/specs/math.test.js
+++ tests/specs/math.test.js
@@ -2,6 +2,30 @@
 	var math = base.math;
 	
 	describe("math", function () {
+		it(".clamp()", function () {
+			expect(typeof math.clamp).toBe('function');
+			for (var i = -2; i < 5; ++i) {
+				for (var min = -2; min < 3; ++min) {
+					for (var max = min + 1; max < 4; ++max) {
+						var clamped = math.clamp(i, min, max);
+						expect(clamped).not.toBeLessThan(min);
+						expect(clamped).not.toBeGreaterThan(max);
+					}
+				}
+			}
+		}); // clamp().
+		
+		it(".sign()", function () {
+			expect(typeof math.sign).toBe('function');
+			for (var i = -3; i < 4; ++i) {
+				expect(math.sign(i)).toBe(i < 0 ? -1 : i > 0 ? +1 : 0);
+			}
+			expect(math.sign(-Infinity)).toBe(-1);
+			expect(math.sign(+Infinity)).toBe(+1);
+			expect(math.sign(NaN)).toBeNaN();
+			expect(math.sign('a')).toBeNaN();
+		}); // sign().
+		
 		it(".factorial()", function () {
 			expect(typeof math.factorial).toBe('function');
 			[[0,1], [1,1], [2,2], [3,6], [4,24], [5,120], [6,720]].forEach(function (test) {
@@ -9,6 +33,8 @@
 			});
 			expect(math.factorial(-1)).toBeNaN();
 		}); // factorial().
+		
+		
 		
 	}); //// describe math.
 });
