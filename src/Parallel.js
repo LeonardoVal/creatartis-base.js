cf5946fd79f41c4e95b64b9667e0aac9358871b4
Leonardo Val <leonardo.val@creatartis.com>
2015-07-31 13:18:08 -0300

    Parallel.loadModule()
    A function to load a module in the webworker.

--- src/Parallel.js
+++ src/Parallel.js
@@ -1,12 +1,10 @@
 ﻿/** # Parallel
 
-Wrapper for standard web workers, that includes bootstraping and a future 
-oriented interface.
+Wrapper for standard web workers, that includes bootstraping and a future oriented interface.
 */
 var Parallel = exports.Parallel = declare({
-	/** The constructor may take a worker instance to deal with. If not given,
-	a new worker is build using `newWorker()`. If given, it must be properly
-	initialized.
+	/** The constructor may take a worker instance to deal with. If not given, a new worker is build 
+	using `newWorker()`. If given, it must be properly initialized.
 	*/
 	constructor: function Parallel(worker) {
 		if (!worker) {
@@ -16,9 +14,8 @@ var Parallel = exports.Parallel = declare({
 		this.worker = worker;
 	},
 	
-	/** `newWorker()` builds a new web worker. Loading `creatartis-base` in its 
-	environment. Sets up a message handler that evaluates posted messages as 
-	code, posting the results back.
+	/** `newWorker()` builds a new web worker. Loading `creatartis-base` in its environment. Sets up
+	a message handler that evaluates posted messages as code, posting the results back.
 	*/
 	"static newWorker": function newWorker() {
 		var src = 'self.base = ('+ exports.__init__ +')();'+
@@ -35,8 +32,8 @@ var Parallel = exports.Parallel = declare({
 		return new Worker(URL.createObjectURL(blob));
 	},	
 	
-	/** The handler for the `worker.onmessage` event is the `__onmessage__(msg)`
-	method. It deals with the futures issued by `run()`.
+	/** The handler for the `worker.onmessage` event is the `__onmessage__(msg)` method. It deals 
+	with the futures issued by `run()`.
 	*/
 	__onmessage__: function __onmessage__(msg) {
 		var future = this.__future__;
@@ -57,8 +54,8 @@ var Parallel = exports.Parallel = declare({
 	
 	/** `run(code)` sends the code to run in the web worker in parallel.
 	
-	Warning! This method will raise an error if it is called while a previous 
-	execution is still running.
+	Warning! This method will raise an error if it is called while a previous execution is still 
+	running.
 	*/
 	run: function run(code) {
 		if (this.__future__) {
@@ -67,16 +64,32 @@ var Parallel = exports.Parallel = declare({
 		this.__future__ = new Future();
 		this.worker.postMessage(code +'');
 		return this.__future__;
-	},
+	}, 
 	
-	/** A _"static"_ version of `run(code)` is provided also. It creates a web 
-	worker to run this code in parallel, and returns a future for its result. 
-	After its finished the web worker is terminated.
+	/** A _"static"_ version of `run(code)` is provided also. It creates a web worker to run this 
+	code in parallel, and returns a future for its result. After its finished the web worker is 
+	terminated.
 	*/
 	"static run": function run(code) {
 		var parallel = new Parallel();
 		return parallel.run(code).always(function () {
 			parallel.worker.terminate();
+		});
+	},
+	
+	/** `loadModule` loads a module in the worker. The module has to have a `__name__`, an 
+	`__init__` function that builds the module and a `__dependencies__` array of modules.
+	*/
+	loadModule: function loadModule(module, recursive) {
+		var parallel = this;
+		return Future.sequence(recursive ? module.__dependencies__ : [], function (dep) {
+			return parallel.loadModule(dep, recursive);
+		}).then(function () {
+			return parallel.run('self.'+ module.__name__ +' || (self.'+ module.__name__ +'=('+ 
+				module.__init__ +')('+ 	module.__dependencies__.map(function (dep) {
+					return dep.__name__;
+				}).join(',') +')), "OK"'
+			);
 		});
 	}
 }); // declare Parallel.
