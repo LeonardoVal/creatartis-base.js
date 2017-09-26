975a062614e9c75e73068603c4f51cd261fc4294
Leonardo Val <leonardo.val@creatartis.com>
2017-09-26 12:01:47 -0300

    Updated `creatartis-grunt`

--- tests/require-config.js
+++ tests/require-config.js
@@ -1,11 +1,25 @@
 // Generated code, please do NOT modify.
 (function () { "use strict";
-	var config = {
-		"paths": {
-			"creatartis-base": "../build/creatartis-base",
-			"sermat": "../node_modules/sermat/build/sermat-umd"
+	define([], function () {
+		var config = {
+			"paths": {
+				"creatartis-base": "../build/creatartis-base",
+				"sermat": "../node_modules/sermat/build/sermat-umd"
+			}
+		};
+		if (window.__karma__) {
+			config.baseUrl = '/base';
+			for (var p in config.paths) {
+				config.paths[p] = config.paths[p].replace(/^\.\.\//, '/base/');
+			}
+			config.deps = Object.keys(window.__karma__.files) // Dynamically load all test files
+				.filter(function (file) { // Filter test modules.
+					return /\.test\.js$/.test(file);
+				}).map(function (file) { // Normalize paths to RequireJS module names.
+					return file.replace(/^\/base\/(.*?)\.js$/, '$1');
+				});
 		}
-	};
-	require.config(config);
-	console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+		require.config(config);
+		console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+	});
 })();
