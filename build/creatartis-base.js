d571e2c51d676443d705b2835e2754a74db4cf6a
Leonardo Val <leonardo.val@creatartis.com>
2018-08-20 12:36:10 -0300

    New build with v0.1.9-alpha

--- build/creatartis-base.js
+++ build/creatartis-base.js
@@ -94,46 +94,6 @@ var copy = exports.copy = function copy(objTo) {
 	return objTo;
 };
 
-/** # Polyfill
-
-This part of the library contains all code meant to equalize Javascript 
-execution environments, to provide some sort of forward compatibility.
-*/ 
-
-/** Some versions of Opera and Internet Explorer do not support 
-[Function.bind](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind).
-*/
-if (!Function.prototype.bind) {
-	Function.prototype.bind = function bind(_this) {
-		if (typeof this !== "function") {
-			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-		}
-		if (arguments.length < 1) {
-			return this;
-		}
-		var args = Array.prototype.slice.call(arguments, 1), 
-			fToBind = this,
-			F_NOP = function () {},
-			fBound = function () {
-				return fToBind.apply(_this, args.concat(Array.prototype.slice.call(arguments)));
-			};
-		F_NOP.prototype = this.prototype;
-		fBound.prototype = new F_NOP();
-		return fBound;
-	};
-}
-
-/** [String.repeat](http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat)
-concatenates a string a given amount of times.
-*/
-if (!String.prototype.repeat) {
-	String.prototype.repeat = function repeat(n) {
-		n = n | 0;
-		return n <= 0 ? "" : n & 1 ? this + this.repeat(n - 1) : (this + this).repeat(n >> 1);
-	};
-}
-
-
 /** # Objects
 
 OOP related functions and definitions.
@@ -260,155 +220,97 @@ var declare = objects.declare;
 
 Text manipulation functions and definitions.
 */
-var XML_ENTITIES = { 
-		'<': '&lt;', 
-		'>': '&gt;', 
-		'&': '&amp;', 
-		'"': '&quot;', 
-		"'": '&apos;' 
-	},
-	Text = exports.Text = declare({
-	/** Text is similar to Java's [`StringBuilder`](http://docs.oracle.com/javase/7/docs/api/java/lang/StringBuilder.html), 
-	but with extended formatting features.
-	*/
-	constructor: function Text() {
-		this.clear();
-	},
-	
-	/** `clear()` empties the text buffer, but returns the previous content.
-	*/
-	clear: function clear() {
-		var text = this.text;
-		this.text = '';
-		return text;
-	},
-	
-	/** `add(...strings)` concatenates all arguments conversions to string to the buffer.
-	*/
-	add: function add() {
-		for (var i = 0; i < arguments.length; i++) {
-			this.text += arguments[i];
-		}
-	},
-	
-	/** The default conversion to string returns the content of the buffer.
-	*/
-	toString: function toString() {
-		return this.text;
-	},
-	
-	// ## Formatting, encoding and decoding ########################################################
-	
-	// ### XML (and HTML for most intends and purposes) ############################################
-	
-	/** `escapeXML(str)` returns the string with XML reserved characters replaced by the 
-	corresponding character entities.
-	*/
-	escapeXML: function escapeXML(str) {
-		var XML_ENTITIES = this.XML_ENTITIES;
-		return (str +'').replace(/[&<>"']/g, function (c) {
-			return XML_ENTITIES[c];
-		});
-	},
-	
-	/** The XML character entities are defined in `XML_ENTITIES`:
-	*/
-	XML_ENTITIES: XML_ENTITIES,
-	'static XML_ENTITIES': XML_ENTITIES,
 
-	/** `addXML(...str)` appends all arguments string conversions after applying `escapeXML()`.
-	*/
-	addXML: function addXML() {
-		for (var i = 0; i < arguments.length; i++) {
-			this.text += this.escapeXML(arguments[i]);
-		}
-	},
-	
-	// ### Regular expressions #####################################################################
-	
-	/** `escapeRegExp(str)` returns the `str` string with the reserved characters of regular 
-	expressions escaped with `'\'`.
-	*/
-	escapeRegExp: function escapeRegExp(str) {
-		return (str +'').replace(/[\-\[\]{}()*+?.^$\\]/g, '\\$&');
-	},
-	
-	// ### Dates ###################################################################################
-	
-	/** `formatDate(date=now, format=Date.toString, useUTC=false)` formats a Date. The `format` 
-	string  may use `y` for year, `m` for month, `d` for day (in month), `h` for hour (24), `H` for
-	hour (am/pm), `n` for minutes, `s` for seconds, `S` for milliseconds, and `a` or `A` for am/pm.
-	*/
-	formatDate: function formatDate(date, format, useUTC) {
-		date = date || new Date();
-		var lpad = Text.lpad;
-		return !format ? date.toString() : format.replace(/(y+|m+|d+|h+|H+|n+|s+|S+|a+|A+|"[^"]*")/g, 
-			function (match) {
-				switch (match.charAt(0)) {
-				case 'y': return lpad((useUTC ? date.getUTCFullYear() : date.getFullYear()) +'', match.length, '0');
-				case 'm': return lpad(((useUTC ? date.getUTCMonth() : date.getMonth()) + 1) +'', match.length, '0');
-				case 'd': return lpad((useUTC ? date.getUTCDate() : date.getDate()) +'', match.length, '0');
-				case 'h': return lpad((useUTC ? date.getUTCHours() : date.getHours()) +'', match.length, '0');
-				case 'H': return lpad((useUTC ? date.getUTCHours() : date.getHours()) % 12 +'', match.length, '0');
-				case 'n': return lpad((useUTC ? date.getUTCMinutes() : date.getMinutes()) +'', match.length, '0');
-				case 's': return lpad((useUTC ? date.getUTCSeconds() : date.getSeconds()) +'', match.length, '0');
-				case 'S': return lpad((useUTC ? date.getUTCMilliseconds() : date.getMilliseconds()) +'', match.length, '0');
-				case 'a': return ['am','pm'][~~((useUTC ? date.getUTCHours() : date.getHours()) / 12)].substr(0, match.length);
-				case 'A': return ['AM','PM'][~~((useUTC ? date.getUTCHours() : date.getHours()) / 12)].substr(0, match.length);
-				case '"': return match.substr(1, match.length-2);
-				default: return match;
-				}
-			});
-	},
-	
-	/** `addDate(date=now, format=Date.toString, useUTC=false)` appends the `date` formatted using
-	`formatDate()`.
-	*/
-	addDate: function addDate(date, format, useUTC) {
-		this.text += this.formatDate(date, format, useUTC);
-	},
-	
-	// ## _Static_ members #########################################################################
-	
-	/** `lpad(str, len, pad=' ')` returns a copy of the `str` string padded with `pad` (or space by 
-	default) to the left upto `len` length.
-	*/
-	'static lpad': function lpad(str, len, pad) {
-		if (isNaN(len) || str.length >= len) {
-			return str;
-		} else {
-			pad = (pad || ' ') +'';
-			return (pad.repeat((len - str.length) / pad.length + 1) + str).substr(-len);
-		}
-	},
-
-	/** `rpad(str, len, pad=' ')` returns a copy of the `str` string padded with `pad` (or space by 
-	default) to the right upto `len` length.
-	*/
-	'static rpad': function rpad(str, len, pad) {
-		if (isNaN(len) || str.length >= len) {
-			return str;
-		} else {
-			pad = (pad || ' ') +'';
-			return (str + pad.repeat((len - str.length) / pad.length + 1)).substr(0, len);
-		}
-	},
-	
-	/** `hashCode(str)` calculates a hash number for the given string.
-	*/
-	'static hashCode': function hashCode(str) {
-		var result = 0,
-			len = str.length;
-		for (var i = 0; i < len; ++i) { 
-			result = (result * 31 + str.charCodeAt(i)) & 0x7FFFFFFF;
-		}
-		return result;
+/** `lpad(str, len, pad=' ')` returns a copy of the `str` string padded with `pad` (or space by 
+default) to the left upto `len` length.
+*/
+exports.lpad = function lpad(str, len, pad) {
+	str = str +'';
+	if (isNaN(len) || str.length >= len) {
+		return str;
+	} else {
+		pad = (pad || ' ') +'';
+		return (pad.repeat((len - str.length) / pad.length + 1) + str).substr(-len);
 	}
-}); // declare Text.
+};
 
-Text.escapeXML = Text.prototype.escapeXML;
-Text.escapeRegExp = Text.prototype.escapeRegExp;
-Text.formatDate = Text.prototype.formatDate;
+/** `rpad(str, len, pad=' ')` returns a copy of the `str` string padded with `pad` (or space by 
+default) to the right upto `len` length.
+*/
+exports.rpad = function rpad(str, len, pad) {
+	str = str +'';
+	if (isNaN(len) || str.length >= len) {
+		return str;
+	} else {
+		pad = (pad || ' ') +'';
+		return (str + pad.repeat((len - str.length) / pad.length + 1)).substr(0, len);
+	}
+};
+
+/** `hashCode(str)` calculates a hash number for the given string.
+*/
+exports.hashCode = function hashCode(str) {
+	str = str +'';
+	var result = 0,
+		len = str.length;
+	for (var i = 0; i < len; ++i) { 
+		result = (result * 31 + str.charCodeAt(i)) & 0x7FFFFFFF;
+	}
+	return result;
+};
+
+// ## Formatting, encoding and decoding ############################################################
+
+/** `escapeXML(str)` returns the string with XML reserved characters replaced by the corresponding 
+character entities.
+*/
+exports.escapeXML = (function () {
+	var XML_ENTITIES = {
+			'&': '&amp;', 
+			'<': '&lt;', '>': '&gt;', 
+			'"': '&quot;', "'": '&apos;' 
+		},
+		escapeXML = function escapeXML(str) {
+			return (str +'').replace(/[&<>"']/g, function (c) {
+				return XML_ENTITIES[c];
+			});
+		};
+	escapeXML.XML_ENTITIES = XML_ENTITIES;
+	return escapeXML;
+})();
+
+/** `escapeRegExp(str)` returns the `str` string with the reserved characters of regular expressions
+escaped with `'\'`.
+*/
+exports.escapeRegExp = function escapeRegExp(str) {
+	return (str +'').replace(/[\-\[\]{}()*+?.^$\\]/g, '\\$&');
+};
+
+/** `formatDate(date=now, format=Date.toString, useUTC=false)` formats a Date. The `format` string 
+may use `y` for year, `m` for month, `d` for day (in month), `h` for hour (24), `H` for hour 
+(am/pm), `n` for minutes, `s` for seconds, `S` for milliseconds, and `a` or `A` for am/pm.
+*/
+exports.formatDate = function formatDate(date, format, useUTC) {
+	date = date || new Date();
+	var lpad = Text.lpad;
+	return !format ? date.toString() : format.replace(/(y+|m+|d+|h+|H+|n+|s+|S+|a+|A+|"[^"]*")/g, 
+		function (match) {
+			switch (match.charAt(0)) {
+			case 'y': return lpad((useUTC ? date.getUTCFullYear() : date.getFullYear()) +'', match.length, '0');
+			case 'm': return lpad(((useUTC ? date.getUTCMonth() : date.getMonth()) + 1) +'', match.length, '0');
+			case 'd': return lpad((useUTC ? date.getUTCDate() : date.getDate()) +'', match.length, '0');
+			case 'h': return lpad((useUTC ? date.getUTCHours() : date.getHours()) +'', match.length, '0');
+			case 'H': return lpad((useUTC ? date.getUTCHours() : date.getHours()) % 12 +'', match.length, '0');
+			case 'n': return lpad((useUTC ? date.getUTCMinutes() : date.getMinutes()) +'', match.length, '0');
+			case 's': return lpad((useUTC ? date.getUTCSeconds() : date.getSeconds()) +'', match.length, '0');
+			case 'S': return lpad((useUTC ? date.getUTCMilliseconds() : date.getMilliseconds()) +'', match.length, '0');
+			case 'a': return ['am','pm'][~~((useUTC ? date.getUTCHours() : date.getHours()) / 12)].substr(0, match.length);
+			case 'A': return ['AM','PM'][~~((useUTC ? date.getUTCHours() : date.getHours()) / 12)].substr(0, match.length);
+			case '"': return match.substr(1, match.length-2);
+			default: return match;
+			}
+		});
+};
 
 
 /** # Math
